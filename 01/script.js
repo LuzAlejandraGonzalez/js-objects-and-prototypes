795a6482265983ed2ee14942786f5d9ea7cda6b5
LuzAlejandraGonzalez <lgonzalez@unitedvirtualities.com>
2019-12-20 10:36:49 -0700

    Module 1: The Dynamic Nature of JavaScript

--- 01/script.js
+++ 01/script.js
@@ -1,6 +1,20 @@
 //display("Hello World");
 
 'use strict';
-var cat = {name:'Fluffy', color: 'White'}
 
+/* var cat = {name:'Fluffy', color: 'White'}
+cat.age = 3;
+cat.speak = function(){ display("Meooow")}
 display(cat.name);
+display(cat.age); */
+
+var cat = {
+    name:'Fluffy',
+    color: 'White',
+    speak: function(){ display("Meooow")}
+}
+
+cat.age = 3;
+cat.speak();
+
+
