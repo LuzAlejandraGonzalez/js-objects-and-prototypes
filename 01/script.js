7aea0d8a1eafe42c4aa5db49c8249974c121095d
LuzAlejandraGonzalez <lgonzalez@unitedvirtualities.com>
2019-12-20 11:53:04 -0700

    Module 1: Using Object.create() to Create JavaScript Objects

--- 01/script.js
+++ 01/script.js
@@ -1,13 +1,30 @@
-//'use strict';
+'use strict';
 
-function Cat(){
-    //this.name = 'Fluffy'
-    this.color = 'White'
+/* function Cat(name, color){
+    this.name = name;
+    this.color = color;
 }
 
-var cat = new Cat();
+var cat = new Cat('Fluffy', 'White'); */
+
+var cat = Object.create(Object.prototype,
+    {
+        name:{
+            value: 'Fluffy',
+            enumerable: true,
+            writable: true,
+            configurable: true
+        },
+
+        color: {
+            value: 'White',
+            enumerable:true,
+            writable:true,
+            configurable: true
+        }
+    })
+
 
 display(cat);
-display(window.color); //White
 
 
